aa7b8f69c9b8c0987a3ff81f0da6c02f82ac7744
Carlos Domínguez García <carlosdg97@gmail.com>
2019-01-09 18:04:27 +0000

    Updates

---
+++ precache-manifest.104274e456362db2e923d48b36af98a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5e57c592fde4cedc493",
+    "url": "/HC/static/js/runtime~main.d5e57c59.js"
+  },
+  {
+    "revision": "7dd1a013638cd8d6ae6f",
+    "url": "/HC/static/js/main.7dd1a013.chunk.js"
+  },
+  {
+    "revision": "8fd2dd30d1ad5f7ab3c2",
+    "url": "/HC/static/js/1.8fd2dd30.chunk.js"
+  },
+  {
+    "revision": "7dd1a013638cd8d6ae6f",
+    "url": "/HC/static/css/main.977a8323.chunk.css"
+  },
+  {
+    "revision": "c4295e38f69cf2c262b18c725736a86c",
+    "url": "/HC/index.html"
+  }
+];
